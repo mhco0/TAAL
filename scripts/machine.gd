28f146ac39ab0378855234c244d28cce56f03089
mhco0 <mhco@cin.ufpe.br>
2023-02-17 12:18:40 -0300

    Adding input ui.

--- scripts/machine.gd
+++ scripts/machine.gd
@@ -5,6 +5,7 @@ var _base: int = 2
 var _mantisse_len: int = 0
 var _exp_min: int = 0
 var _exp_max: int = 1
+var _use_rounding: bool = false # Uses truncate
 
 ##
 # @brief Initializes a new machine using the numbers of the @p base with @p mantisse.
@@ -13,14 +14,25 @@ var _exp_max: int = 1
 # @param exp1 The minimum value for the expoent
 # @param exp2 The maximum value for the expoent
 ##
-func _init(base, mantisse, exp1, exp2):
+func _init(base, mantisse, exp1, exp2, use_rounding):
 	assert(exp1 < exp2, "The first exp parameter should be the minimum value")
 	_base = base
 	_mantisse_len = mantisse
 	_exp_min = exp1
 	_exp_max = exp2
+	_use_rounding = use_rounding
 
-func represent(number: int) -> MachineNumber:
+
+
+##
+# @brief Get the number of representable elements that can be represent by this machine
+# @return The number of representable elements
+##
+func representable_elements() -> int:
+	return int (2 * (_base - 1) * pow(_base, _mantisse_len - 1) * (_exp_max - _exp_min + 1)) + 1
+
+# Divide the number by its base and get the full representation in the mantisse 
+func represent(number: float) -> MachineNumber:
 	return MachineNumber.new(0, [], 0, false)
 
 func add(n1: MachineNumber, n2: MachineNumber) -> MachineNumber:
@@ -35,8 +47,9 @@ func mult(n1: MachineNumber, n2: MachineNumber) -> MachineNumber:
 func div(n1: MachineNumber, n2: MachineNumber) -> MachineNumber:
 	return MachineNumber.new(0, [], 0, false)
 
-func machine_sin(n1: int) -> MachineNumber:
+func machine_sin(n1: float) -> MachineNumber:
+	
 	return MachineNumber.new(0, [], 0, false)
 	
-func machine_cos(n1: int) -> MachineNumber:
+func machine_cos(n1: float) -> MachineNumber:
 	return MachineNumber.new(0, [], 0, false)
---
+++ scripts/ui/build_machine.gd
@@ -0,0 +1,26 @@
+extends Control
+
+
+onready var base_input = $CenterContainer/PanelContainer/VBoxContainer/BaseLabel/SpinBox
+onready var mantisse_input = $CenterContainer/PanelContainer/VBoxContainer/MantisseLabel/SpinBox
+onready var exp1_input = $CenterContainer/PanelContainer/VBoxContainer/Exp1Label/SpinBox
+onready var exp2_input = $CenterContainer/PanelContainer/VBoxContainer/Exp2Label/SpinBox
+onready var use_rounding_input = $CenterContainer/PanelContainer/VBoxContainer/UseRounding/CheckBox
+onready var create_button = $CenterContainer/PanelContainer/VBoxContainer/CreateButton
+
+func _ready():
+	var error = create_button.connect("pressed", self, "_on_create_button_pressed")
+	if (error != OK):
+		print("Couldn't connect signal pressed in create_button on BuildMachine scene")
+
+
+func _on_create_button_pressed():
+	var base: int = int(base_input.get_line_edit().text)
+	var mantisse_len: int = int(mantisse_input.get_line_edit().text)
+	var exp1: int = int(exp1_input.get_line_edit().text)
+	var exp2: int = int(exp2_input.get_line_edit().text)
+	var use_rounding: bool = use_rounding_input.pressed
+	
+	var machine: Machine = Machine.new(base, mantisse_len, exp1, exp2, use_rounding)
+	
+	print("Count of representable numbers on machine: %d" % [machine.representable_elements()])
